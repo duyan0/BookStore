569710223436d93c4951b345f821a2c8322550a7
Võ Duy Ân <crandi21112004@gmail.com>
2025-07-28 02:17:52 +0700

    Refactor code structure for improved readability and maintainability

--- BookStore.API/obj/Debug/net9.0/BookStore.API.AssemblyInfo.cs
+++ BookStore.API/obj/Debug/net9.0/BookStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e08c617684eb5cef6c5f0fb2e1d1e4a7632142eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6df710384c6417489837c1a9a6b4681de9a7721")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
